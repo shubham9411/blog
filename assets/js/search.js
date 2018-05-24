2e95318d21019df8543122fe16188ce25531c097
karannagpal96 <karan@NoSkillGuys-MacBook-Pro.local>
2018-05-24 13:06:20 +0530

    working on search operation

--- assets/js/search.js
+++ assets/js/search.js
@@ -45,8 +45,8 @@
   }
 
   var searchTerm = getQueryVariable('query');
-// above line returns all the words typed in by the user, seperated by space
-// datatype of searchTerm is string, not array
+  // above line returns all the words typed in by the user, seperated by space
+  // datatype of searchTerm is string, not array
 
 
 
@@ -61,7 +61,7 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
 
-  // code from bakery store starts
+  // bakery code starts
     /*
     var idx = lunr(function () {
       this.field('id');
@@ -71,8 +71,102 @@
       this.field('content');
     });
     */
-  // bakery blog code ends
+  // bakery code ends
 
+
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('category', { boost: 100});
+      this.field('title', { boost: 10 });
+      this.field('content');
+
+      //below line is not working
+      //this.ref('url');
+    });
+
+
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'author': window.store[key].author,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+})();
+
+
+
+//// for console testing only ////
+
+//// copy paste the below code in console one section at a time ////
+
+//// then see the value of searchTerm, searchResults, results, windows.store ////
+
+
+/*
+
+============================================
+
+// work starts from here,
+// this is step 1
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+
+  var searchTerm = getQueryVariable('query');
+// above line returns all the words typed in by the user, seperated by space
+// datatype of searchTerm is string, not array
+
+============================================
+
+function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+
+        //the heading first
+        appendString += '<li><a href="/blog' + item.url + '"><h3>' + item.title + '</h3></a>';
+
+        //content of the post, truncated to first 150 characters
+        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+      }
+
+      searchResults.innerHTML = appendString;
+    }
+
+    else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+
+  }
+
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
 
     var idx = lunr(function () {
       this.field('id');
@@ -98,4 +192,4 @@
       displaySearchResults(results, window.store); // We'll write this in the next section
     }
   }
-})();
+*/
